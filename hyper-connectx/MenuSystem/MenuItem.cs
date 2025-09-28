262b5e4f6bca9c56e74de74a68f75d16531ab26b
Johannes Paju <jopaju@taltech.ee>
2025-09-28 18:58:47 +0300

    lapakast pcsse

--- hyper-connectx/MenuSystem/MenuItem.cs
+++ hyper-connectx/MenuSystem/MenuItem.cs
@@ -2,10 +2,15 @@
 
 public class MenuItem
 {
-    public string Label  { get; set; }
-    public string Key { get; set; }
-    // thing that should happen
+    public string Label { get; set; } = default!;
 
+    public string Key { get; set; } = default!;
+    
+    
+    // exit, return to prev and return to main do not have actions
+    // all other menu items should have actions
+    public Action<string>? RunThisWhenMenuItemIsSelected { get; set; }
+    
     public override string ToString()
     {
         return $"{Key}: {Label}";
