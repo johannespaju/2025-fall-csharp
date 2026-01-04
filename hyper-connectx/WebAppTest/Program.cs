40212eb1a2bf8be01103bcb1ac8f2ab978deef27
Johannes Paju <jopaju@taltech.ee>
2026-01-04 15:53:14 +0200

    test

---
+++ hyper-connectx/WebAppTest/Program.cs
@@ -0,0 +1,46 @@
+using DAL;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
+                       throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+var homeDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+homeDirectory = homeDirectory + Path.DirectorySeparatorChar;
+
+connectionString = connectionString.Replace("<db_file>", $"{homeDirectory}app.db");
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+app.MapRazorPages()
+    .WithStaticAssets();
+
+app.Run();
