d1ec1af36fbbd4b397d4f6de41df6f1cf19f0731
Johannes Paju <jopaju@taltech.ee>
2026-01-16 18:11:17 +0200

    .

--- Exam/BLL/Validators/RentalValidator.cs
+++ Exam/BLL/Validators/RentalValidator.cs
@@ -1,17 +1,12 @@
 using BLL.Enums;
-using BLL.Interfaces;
 using FluentValidation;
 
 namespace BLL.Validators;
 
 public class RentalValidator : AbstractValidator<Rental>
 {
-    private readonly IAvailabilityService _availabilityService;
-    
-    public RentalValidator(IAvailabilityService availabilityService)
+    public RentalValidator()
     {
-        _availabilityService = availabilityService;
-        
         RuleFor(r => r.EndDate)
             .GreaterThanOrEqualTo(r => r.StartDate)
             .WithMessage("End date must be on or after start date");
@@ -29,16 +24,6 @@ public class RentalValidator : AbstractValidator<Rental>
             .Must(BeValidTimeBlock)
             .When(r => r.RentalType == RentalType.FourHour)
             .WithMessage("4-hour rentals must start at 09:00, 13:00, 17:00, or 21:00");
-            
-        RuleFor(r => r)
-            .MustAsync(async (rental, cancellation) => 
-            {
-                var startDateTime = rental.StartDate.ToDateTime(rental.StartTime);
-                var endDateTime = rental.EndDate.ToDateTime(rental.EndTime);
-                return await _availabilityService.IsBikeAvailableAsync(
-                    rental.BikeId, startDateTime, endDateTime);
-            })
-            .WithMessage("Bike is not available for the requested period");
     }
     
     private bool BeValidTimeBlock(TimeOnly time)
